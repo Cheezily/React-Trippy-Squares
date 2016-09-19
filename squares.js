ab12d43904711cef0e1b2734f70bcaf3eb1a9239
cheezily <pm655@yahoo.com>
2016-09-19 16:15:52 -0500

    initial commit

---
+++ squares.js
@@ -0,0 +1,136 @@
+var maxStars = 60;
+var startingStars = 40;
+var starBurst = 15;
+var startX = Math.floor(screen.availWidth/10);
+var startY = Math.floor(screen.availHeight/10)
+var starSpeed = Math.floor(screen.availWidth/500);
+var sizePxGrowth = 3;
+var shadowGrowth = .5;
+var borderGrowth = .1;
+var stars = [];
+var target = document.getElementById("target");
+var dragX = 0;
+var dragY = 0;
+
+var delay = 10;
+var starChance = .2;
+var movementMultiplier = .013;
+
+var Output = React.createClass({
+  render: function() {
+    var starMap = this.props.starMap;
+    //console.log(starMap);
+    return (
+      <div>
+        {starMap.map(function(thisStar, key) {
+          return (<div key={key + 1} className='star' style={thisStar.style}></div>)
+        })
+        }
+      </div>
+    );
+  }
+
+});
+
+function makeStars(override) {
+
+  //i only want it to make a star on a small number of attempts
+  if(Math.random() < starChance || override) {
+    var direction = Math.floor(Math.random() * 360);
+    var color = "rgb(" + Math.floor(Math.random() * 255) +
+                ", " + Math.floor(Math.random() * 255) +
+                ", " + Math.floor(Math.random() * 255) +
+                ")";
+
+    var star = {xPos: startX,
+                yPos: startY,
+                width: 1,
+                height: 1,
+                direction: direction,
+                moveX: Math.floor(Math.cos(direction) * starSpeed) + Math.floor(Math.random() * 5 - 2.5),
+                moveY: Math.floor(Math.sin(direction) * starSpeed) + Math.floor(Math.random() * 5 - 5),
+                color: color,
+                shadow: .5,
+                border: 1,
+                style: ''
+              };
+  stars.push(star);
+  }
+}
+
+
+function showStars() {
+  var starStyle = '';
+
+  for (var i = 0; i < stars.length; i++) {
+
+    starStyle = {top: stars[i].yPos + "px",
+                left: stars[i].xPos + "px",
+                width: Math.floor(stars[i].width) + "px",
+                height: Math.floor(stars[i].height) + "px",
+                background: stars[i].color,
+                border: stars[i].border + "px solid #000",
+                boxShadow: Math.floor(stars[i].shadow) +"px "+ Math.floor(stars[i].shadow) +"px " + Math.floor(stars[i].shadow) + "px #000"
+    };
+
+    moveStar(i, starStyle);
+    deleteCheck(stars[i], i);
+  }
+
+  //target.innerHTML = starDivs;
+  ReactDOM.render(<Output starMap={stars} />, document.getElementById('target'));
+
+  setTimeout(function() {
+    showStars();
+  }, delay);
+
+  if (stars.length < maxStars) {
+    makeStars();
+  }
+}
+
+
+function moveStar(i, starStyle) {
+  stars[i].xPos += stars[i].moveX + dragX * stars[i].width;
+  stars[i].yPos += stars[i].moveY + dragY * stars[i].width;
+  stars[i].width += sizePxGrowth;
+  stars[i].height += sizePxGrowth;
+  stars[i].shadow += shadowGrowth;
+  stars[i].border += borderGrowth;
+  stars[i].style = starStyle;
+}
+
+
+function deleteCheck(star, i) {
+  if (star.xPos > screen.availWidth * 1.3 ||
+      star.yPos > screen.availHeight * 1.3 ||
+      star.xPos < -screen.availHeight * 1.3 ||
+      star.yPos < -screen.availWidth * 1.3 ||
+      star.width > screen.availHeight * .6) {
+        stars.splice(i, 1);
+      }
+}
+
+document.onmousemove = function(event) {
+
+  if (event.clientX > startX) {dragX = movementMultiplier;}
+  if (event.clientX < startX) {dragX = -movementMultiplier;}
+  if (event.clientY > startY) {dragY = movementMultiplier;}
+  if (event.clientY < startY) {dragY = -movementMultiplier;}
+  if (event.clientX == startX) {dragX = dragX * .5;}
+  if (event.clientY == startY) {dragY = dragY * .5;}
+  startX = event.clientX;
+  startY = event.clientY;
+}
+
+target.onmousedown = function() {
+  for (var i = 0; i < starBurst; i++) {
+    makeStars(true);
+  }
+}
+
+for (var i = 0; i < startingStars; i++) {
+  makeStars(true);
+}
+
+showStars();
